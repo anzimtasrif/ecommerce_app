cbf40e3165448df3e1a744c034c99247f3d1ff45
anzimtasrif <anzimtasrifathoi@gmail.com>
2026-02-28 01:34:13 +0600

    first commit

---
+++ lib/pages/home.dart
@@ -0,0 +1,45 @@
+import 'package:ecommerce_app/widget/widget_support.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        margin: EdgeInsets.only(top: 50.0, left: 10.0, right: 20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text("Hello Anzim,",
+              style:AppWidget.boldTextFieldstyle()
+            ),
+            Container(
+              padding: EdgeInsets.all(3),
+              decoration: BoxDecoration(color: Colors.black, borderRadius: BorderRadius.circular(8)),
+              child: Icon(Icons.shopping_cart, color: Colors.white,),
+            )
+          ],
+        ),
+          SizedBox(height: 20.0,),
+          Text("Delicious Food",
+              style:AppWidget.HeadlineTextFieldstyle()
+          ),
+            Text("Discover and Get Great Food",
+                style:AppWidget.LightTextFieldstyle()
+            ),
+        ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widget/widget_support.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AppWidget{
+
+  static TextStyle boldTextFieldstyle(){
+    return TextStyle(
+        color: Colors.black,
+        fontSize: 20.0,
+        fontWeight: FontWeight.bold);
+  }
+  static TextStyle HeadlineTextFieldstyle(){
+    return TextStyle(
+        color: Colors.black,
+        fontSize: 24.0,
+        fontWeight: FontWeight.bold);
+  }
+  static TextStyle LightTextFieldstyle(){
+    return TextStyle(
+        color: Colors.black38,
+        fontSize: 15.0,
+        fontWeight: FontWeight.w500);
+  }
+}
