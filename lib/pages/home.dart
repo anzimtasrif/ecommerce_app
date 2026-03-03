d30905cfc4e01c479b25641c444df2fbaa355d41
anzimtasrif <anzimtasrifathoi@gmail.com>
2026-03-03 18:56:04 +0600

    New updated

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -13,31 +13,64 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        margin: EdgeInsets.only(top: 50.0, left: 10.0, right: 20.0),
+        margin: const EdgeInsets.only(top: 50.0, left: 10.0, right: 20.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text("Hello Anzim,",
-              style:AppWidget.boldTextFieldstyle()
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Hello Anzim,", style: AppWidget.boldTextFieldstyle()),
+                Container(
+                  padding: const EdgeInsets.all(3),
+                  decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(8)),
+                  child: const Icon(
+                    Icons.shopping_cart,
+                    color: Colors.white,
+                  ),
+                )
+              ],
             ),
-            Container(
-              padding: EdgeInsets.all(3),
-              decoration: BoxDecoration(color: Colors.black, borderRadius: BorderRadius.circular(8)),
-              child: Icon(Icons.shopping_cart, color: Colors.white,),
-            )
-          ],
-        ),
-          SizedBox(height: 20.0,),
-          Text("Delicious Food",
-              style:AppWidget.HeadlineTextFieldstyle()
-          ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            Text("Delicious Food", style: AppWidget.HeadlineTextFieldstyle()),
             Text("Discover and Get Great Food",
-                style:AppWidget.LightTextFieldstyle()
+                style: AppWidget.LightTextFieldstyle()),
+            const SizedBox(height: 20.0),
+            Row(
+              children: [
+                Material(
+                  elevation: 5.0,
+                  borderRadius: BorderRadius.circular(10),
+                  child: Container(
+                    padding: const EdgeInsets.all(10),
+                    child: Image.asset(
+                      "images/ice-cream.png",
+                      height: 50,
+                      width: 50,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Material(
+                  elevation: 5.0,
+                  borderRadius: BorderRadius.circular(10),
+                  child: Container(
+                    padding: const EdgeInsets.all(10),
+                    child: Image.asset(
+                      "images/ice-cream.png",
+                      height: 50,
+                      width: 50,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                )
+              ],
             ),
-        ],
+          ],
         ),
       ),
     );
